1ed3c28b99d532a3041c9403813641349f5173fa
tolyakryv <90652628+tolyakryv@users.noreply.github.com>
2022-11-13 01:52:56 +0200

    addType

--- app.js
+++ app.js
@@ -7,3 +7,66 @@ function add(num1, num2) {
 button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+var age = 50;
+var names = 'Max';
+var toggle = true;
+var empty;
+empty = null;
+var notInitialize = undefined;
+var callback;
+callback = function (a) { return 100 + a; };
+// -----------------
+var anything;
+anything = -20;
+anything = 'Text';
+anything = {};
+var some;
+some = 'Text';
+var str;
+str = some;
+var person;
+person = ['Max', 21];
+var StatusLoad;
+(function (StatusLoad) {
+    StatusLoad[StatusLoad["LOADING"] = 0] = "LOADING";
+    StatusLoad[StatusLoad["READY"] = 1] = "READY";
+})(StatusLoad || (StatusLoad = {}));
+;
+var page = {
+    load: StatusLoad.READY
+};
+if (page.load === StatusLoad.READY) {
+    console.log("завантажено");
+}
+if (page.load === StatusLoad.LOADING) {
+    console.log('завантажується');
+}
+var stringOrNumber;
+stringOrNumber = "num";
+var arbait;
+arbait = 'enable';
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01'
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close'
+};
--- app.ts
+++ app.ts
@@ -9,3 +9,84 @@ function add(num1: number, num2: number) {
 button?.addEventListener("click", function() {
   console.log(add(+input1.value, +input2.value));
 });
+let age:number = 50;
+let names: string = 'Max';
+let toggle:boolean = true;
+let empty:null
+  empty= null;
+let notInitialize: undefined = undefined;
+let callback: (param: number) => number 
+callback = (a: number) => { return 100 + a };
+ 
+// -----------------
+let anything:any
+  anything = -20;
+anything = 'Text';
+anything = {};
+
+let some:string;
+some = 'Text';
+let str: string;
+str = some;
+
+let person:[string, number]
+person = ['Max', 21];
+  
+enum StatusLoad { LOADING, READY };
+
+const page = {
+ load: StatusLoad.READY
+}
+if (page.load === StatusLoad.READY) {
+  console.log("завантажено")
+}
+if (page.load === StatusLoad.LOADING) {
+  console.log('завантажується')
+}
+let stringOrNumber: string | number
+stringOrNumber = "num"
+
+let arbait: 'enable' | 'disable'
+arbait = 'enable'
+
+function showMessage(message:string):void {
+  console.log(message);
+}
+
+
+function calc(num1:number, num2:number):number {
+  return num1 + num2;
+}
+
+function customError():never {
+  throw new Error('Error');
+}
+
+// -------------------!SECTION
+type pageType= {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: 'open' | 'close',
+  details?: {
+    createAt: string,
+    updateAt:string
+  }
+}
+const page1:pageType = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  }
+}
+
+const page2:pageType = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
