fcfd22dfc212763693bad89951ccda21f7495e8e
tolyakryv <90652628+tolyakryv@users.noreply.github.com>
2022-11-22 13:15:44 +0200

    finish b04

--- src/b04.ts
+++ src/b04.ts
@@ -14,3 +14,32 @@ class Person {
         return this.key
     }
 }
+abstract class House{
+    protected door = false;
+    private tenant: Person[] = [];
+    constructor(protected key: Key) { }
+    
+    comIn(person: Person): void{
+        if (!this.door) {
+            throw new Error('door closed')
+        }
+        this.tenant.push(person);
+        console.log('person in house')
+    }
+    abstract openDoor(key:Key):boolean
+}
+
+class MyHouse extends House{
+    openDoor(key: Key) {
+        if (key.getSignature() !== this.key.getSignature()) {
+            throw new Error('false key')
+        }
+        return this.door=true
+    }
+}
+const key = new Key;
+
+const house = new MyHouse(key);
+const persons = new Person(key);
+house.openDoor(persons.getKey())
+house.comIn(persons)
